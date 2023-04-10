61fdbae47bcab35a975226d53dad982f34a17007
Anthony Ardito <anthony.ardito@gmail.com>
2023-04-10 14:15:55 -0400

    Add inputs in Define Users section to update name/email

--- src/DefineUsers.tsx
+++ src/DefineUsers.tsx
@@ -1,8 +1,6 @@
 import { useCallback, useEffect, useState } from 'react';
 import { ParsedUser } from './parseUserEmails';
 import Button, { ButtonType } from './components/Button';
-import Select from './components/Select';
-import { type SingleValue } from 'react-select';
 import TableRow from './components/Table/TableRow';
 import Table from './components/Table/Table';
 import TableHeaderCell from './components/Table/TableHeaderCell';
@@ -12,14 +10,14 @@ import SectionHeading from './components/SectionHeading';
 import TableBody from './components/Table/TableBody';
 import TableHead from './components/Table/TableHead';
 import { DisciplineDetailsComponent, RoleDetailsComponent } from './api/types';
-import { disciplines, roles } from './api/mockData';
+import DefineUsersUserRow from './DefineUsersUserRow';
 
 interface Props {
   enabled: boolean;
   parsedUsers: Array<ParsedUser>;
 }
 
-interface DefinedUser {
+export interface DefinedUser {
   firstName: string | null;
   lastName: string | null;
   emailAddress: string;
@@ -51,73 +49,9 @@ const DefineUsers: React.FC<Props> = ({ enabled, parsedUsers }) => {
     });
   }, [parsedUsers]);
 
-  const swapFirstAndLastName = useCallback((idx: number) => {
-    setUsers((users) => {
-      const newUsers = [...users];
-
-      newUsers[idx] = {
-        ...newUsers[idx],
-        firstName: newUsers[idx].lastName,
-        lastName: newUsers[idx].firstName,
-      };
-
-      return newUsers;
-    });
-  }, []);
-
   const handleCancel = useCallback(() => {
     setUsers([]);
   }, []);
-
-  const handleChangeDiscipline =
-    (idx: number) => (discipline: SingleValue<DisciplineDetailsComponent>) => {
-      setUsers((users) => {
-        const updatedUsers = [...users];
-
-        updatedUsers[idx] = {
-          ...updatedUsers[idx],
-          discipline,
-        };
-
-        return updatedUsers;
-      });
-    };
-
-  const handleChangeRole =
-    (idx: number) => (role: SingleValue<RoleDetailsComponent>) => {
-      setUsers((users) => {
-        const updatedUsers = [...users];
-
-        updatedUsers[idx] = {
-          ...updatedUsers[idx],
-          role,
-        };
-
-        return updatedUsers;
-      });
-    };
-
-  const getRoleOptionValue = useCallback(
-    (role: SingleValue<RoleDetailsComponent>) => role?.id ?? '',
-    []
-  );
-
-  const getRoleOptionLabel = useCallback(
-    (role: SingleValue<RoleDetailsComponent>) => role?.payload.name ?? '',
-    []
-  );
-
-  const getDisciplineOptionValue = useCallback(
-    (discipline: SingleValue<DisciplineDetailsComponent>) =>
-      discipline?.id ?? '',
-    []
-  );
-
-  const getDisciplineOptionLabel = useCallback(
-    (discipline: SingleValue<DisciplineDetailsComponent>) =>
-      discipline?.payload.name ?? '',
-    []
-  );
 
   return (
     <Section>
@@ -143,50 +77,12 @@ const DefineUsers: React.FC<Props> = ({ enabled, parsedUsers }) => {
               </TableRow>
             )}
             {users.map((user, idx) => (
-              <TableRow key={user.emailAddress}>
-                <TableDataCell data-label="First" className="break-all">
-                  {user.firstName}
-                </TableDataCell>
-                <TableDataCell className="text-right">
-                  {user.firstName && user.lastName && (
-                    <Button
-                      buttonType={ButtonType.Secondary}
-                      className="!p-1 !px-2 text-sm font-extrabold"
-                      onClick={() => swapFirstAndLastName(idx)}
-                    >
-                      {'Swap First/Last'}
-                    </Button>
-                  )}
-                </TableDataCell>
-                <TableDataCell data-label="Last" className="break-all">
-                  {user.lastName}
-                </TableDataCell>
-                <TableDataCell data-label="Email" className="break-all">
-                  {user.emailAddress}
-                </TableDataCell>
-                <TableDataCell data-label="Discipline">
-                  <Select
-                    aria-label="Discipline"
-                    menuPortalTarget={document.body}
-                    options={disciplines}
-                    onChange={handleChangeDiscipline(idx)}
-                    selectedValue={user.discipline}
-                    getOptionValue={getDisciplineOptionValue}
-                    getOptionLabel={getDisciplineOptionLabel}
-                  />
-                </TableDataCell>
-                <TableDataCell data-label="Role">
-                  <Select
-                    aria-label="Role"
-                    menuPortalTarget={document.body}
-                    options={roles}
-                    onChange={handleChangeRole(idx)}
-                    selectedValue={user.role}
-                    getOptionValue={getRoleOptionValue}
-                    getOptionLabel={getRoleOptionLabel}
-                  />
-                </TableDataCell>
-              </TableRow>
+              <DefineUsersUserRow
+                key={idx}
+                user={user}
+                setUsers={setUsers}
+                idx={idx}
+              />
             ))}
           </TableBody>
         </Table>
