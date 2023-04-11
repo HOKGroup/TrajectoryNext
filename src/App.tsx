f552e9d49564aa7cd6d9f06670f62b827efe1f65
Anthony Ardito <anthony.ardito@gmail.com>
2023-04-11 15:55:33 -0400

    Remove sticky from main header

--- src/App.tsx
+++ src/App.tsx
@@ -18,30 +18,30 @@ function App() {
 
   return (
     <>
-      <div className="sticky top-0 z-10 border-b bg-slate-100 py-1 dark:bg-slate-800">
-        <header className="my-1 flex flex-row items-center justify-between pl-4">
-          <span className="flex items-center">
-            <img src={hokLogo} className="mr-4 h-8 w-8" alt="HOK logo" />
-            <h1 className="whitespace-nowrap text-2xl font-semibold">
-              Trajectory Next
-            </h1>
-          </span>
-          <span>
-            <DarkModeToggle />
-          </span>
-        </header>
-      </div>
-      <main className="container flex grow flex-col gap-6 pb-16 pt-4">
-        <SelectProject
-          setSelectedProject={setSelectedProject}
-          selectedProject={selectedProject}
-        />
-        <UserEmailsInput
-          enabled={!!selectedProject}
-          setParsedUsers={setParsedUsers}
-        />
-        <DefineUsers enabled={!!selectedProject} parsedUsers={parsedUsers} />
-        <ExistingTeam project={selectedProject} services={services} />
+      <header className="flex flex-row items-center justify-between border-b bg-slate-100 py-2 pl-4 dark:bg-slate-800">
+        <span className="flex items-center">
+          <img src={hokLogo} className="mr-4 h-8 w-8" alt="HOK logo" />
+          <h1 className="whitespace-nowrap text-2xl font-semibold">
+            Trajectory Next
+          </h1>
+        </span>
+        <span>
+          <DarkModeToggle />
+        </span>
+      </header>
+      <main className="grow overflow-y-auto pb-16 pt-4">
+        <div className="container flex flex-col gap-6">
+          <SelectProject
+            setSelectedProject={setSelectedProject}
+            selectedProject={selectedProject}
+          />
+          <UserEmailsInput
+            enabled={!!selectedProject}
+            setParsedUsers={setParsedUsers}
+          />
+          <DefineUsers enabled={!!selectedProject} parsedUsers={parsedUsers} />
+          <ExistingTeam project={selectedProject} services={services} />
+        </div>
       </main>
     </>
   );
