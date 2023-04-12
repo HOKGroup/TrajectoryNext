f5fd3d5f41dc5537e1ac4b7f2966dadab08fa97b
Anthony Ardito <anthony.ardito@gmail.com>
2023-04-12 12:28:47 -0400

    Add remove button to Define Users rows

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -3,6 +3,7 @@ import iconSun from '../assets/icons/sun-regular.svg';
 import iconMoon from '../assets/icons/moon-regular.svg';
 import iconSystem from '../assets/icons/display-solid.svg';
 import iconChevronDown from '../assets/icons/chevron-down-solid.svg';
+import iconXMark from '../assets/icons/xmark-solid.svg';
 
 export const IconSun = (props: HTMLProps<HTMLImageElement>) => (
   <img src={iconSun} {...props} />
@@ -18,4 +19,8 @@ export const IconSystem = (props: HTMLProps<HTMLImageElement>) => (
 
 export const IconChevronDown = (props: HTMLProps<HTMLImageElement>) => (
   <img src={iconChevronDown} {...props} />
+);
+
+export const IconXMark = (props: HTMLProps<HTMLImageElement>) => (
+  <img src={iconXMark} {...props} />
 );
